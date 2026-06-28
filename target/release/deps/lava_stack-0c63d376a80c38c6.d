26d967b89225a9497a1298f117f407aa3eeb40ee
drzzln <drzzln@protonmail.com>
2026-06-28 00:02:11 -0400

    chore: sync working tree (fleet git-clean sweep)

--- target/release/deps/lava_stack-0c63d376a80c38c6.d
+++
@@ -1,7 +0,0 @@
-/Users/drzzln/code/github/pleme-io/lava-stack/target/release/deps/lava_stack-0c63d376a80c38c6.d: src/lib.rs
-
-/Users/drzzln/code/github/pleme-io/lava-stack/target/release/deps/liblava_stack-0c63d376a80c38c6.rlib: src/lib.rs
-
-/Users/drzzln/code/github/pleme-io/lava-stack/target/release/deps/liblava_stack-0c63d376a80c38c6.rmeta: src/lib.rs
-
-src/lib.rs:
--- target/release/deps/lava_stack-4a6d0611951bb636.d
+++
@@ -1,5 +0,0 @@
-/Users/drzzln/code/github/pleme-io/lava-stack/target/release/deps/lava_stack-4a6d0611951bb636.d: src/lib.rs
-
-/Users/drzzln/code/github/pleme-io/lava-stack/target/release/deps/lava_stack-4a6d0611951bb636: src/lib.rs
-
-src/lib.rs:
